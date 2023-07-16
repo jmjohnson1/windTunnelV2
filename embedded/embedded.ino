f434116dd86eb9dcbc71455a59def1b60df0a899
jmjohnson1 <106408381+jmjohnson1@users.noreply.github.com>
2023-07-16 17:59:58 -0500

    Added some declarations

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -1,3 +1,28 @@
+// MCP23017 //
+const uint8_t MCP_addr = 0x00;
+
+// Solenoid valve pins //
+const uint8_t sol1_pinMCP = 0;
+const uint8_t sol2_pinMCP = 1;
+const uint8_t sol3_pinMCP = 2;
+const uint8_t sol4_pinMCP = 3;
+const uint8_t sol5_pinMCP = 4;
+const uint8_t sol6_pinMCP = 5;
+const uint8_t sol7_pinMCP = 6;
+const uint8_t sol8_pinMCP = 7;
+const uint8_t sol9_pinMCP = 8;
+const uint8_t sol10_pinMCP = 9;
+const uint8_t sol11_pinMCP = 10;
+const uint8_t sol12_pinMCP = 11;
+const uint8_t sol13_pinMCP = 12;
+const uint8_t sol14_pinMCP = 13;
+const uint8_t sol15_pinMCP = 14;
+const uint8_t sol16_pinMCP = 15;
+const uint8_t sol17_pin = 0;
+const uint8_t sol18_pin = 1;
+const uint8_t sol19_pin = 2;
+const uint8_t sol20_pin = 3;
+
 void setup() {
   // put your setup code here, to run once:
 
