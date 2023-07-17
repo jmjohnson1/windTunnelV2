a7d90e7a2f35541ddab07514b602dd32688b0837
jmjohnson1 <106408381+jmjohnson1@users.noreply.github.com>
2023-07-16 20:29:39 -0500

    Added some logic for switching. More declarations.

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -1,5 +1,9 @@
+#include "src/arduino-mcp23017/src/MCP23017.h"
+#include "src/ams5812/src/ams5812.h"
+
 // MCP23017 //
 const uint8_t MCP_addr = 0x00;
+MCP23017 mcp = MCP23017(MCP_addr);
 
 // Solenoid valve pins //
 const uint8_t sol1_pinMCP = 0;
@@ -18,17 +22,77 @@ const uint8_t sol13_pinMCP = 12;
 const uint8_t sol14_pinMCP = 13;
 const uint8_t sol15_pinMCP = 14;
 const uint8_t sol16_pinMCP = 15;
-const uint8_t sol17_pin = 0;
-const uint8_t sol18_pin = 1;
-const uint8_t sol19_pin = 2;
-const uint8_t sol20_pin = 3;
+const uint8_t sol17_pin = 3;
+const uint8_t sol18_pin = 2;
+const uint8_t sol19_pin = 1;
+const uint8_t sol20_pin = 0;
+
+uint8_t pinArray[20] = {
+	sol1_pinMCP,
+	sol2_pinMCP,
+	sol3_pinMCP,
+	sol4_pinMCP,
+	sol5_pinMCP,
+	sol6_pinMCP,
+	sol7_pinMCP,
+	sol8_pinMCP,
+	sol9_pinMCP,
+	sol10_pinMCP,
+	sol11_pinMCP,
+	sol12_pinMCP,
+	sol13_pinMCP,
+	sol14_pinMCP,
+	sol15_pinMCP,
+	sol16_pinMCP,
+	sol17_pin,
+	sol18_pin,
+	sol19_pin,
+	sol20_pin
+};
+
+
+// AMS 5812 //
+const uint8_t pSensor1_addr = 0x04;
+const uint8_t pSensor2_addr = 0x05;
+bfs::Ams5812 pSensor1(&Wire, pSensor1_addr, bfs::Ams5812::AMS5812_0003_D);
+bfs::Ams5812 pSensor2(&Wire, pSensor2_addr, bfs::Ams5812::AMS5812_0003_D);
+
+// Analog Inputs //
 
 void setup() {
-  // put your setup code here, to run once:
-
+	Serial.begin(9600);
+	Wire.begin();
+	Wire.setClock(400000); // Fast mode
+	if (!pSensor1.Begin()) {
+		Serial.println("Pressure sensor 1 failed to initialize");
+	}
+	if (!pSensor2.Begin()) {
+		Serial.println("Pressure sensor 2 failed to initialize");
+	}
+	mcp.begin();
+	// Configure all pins on MCP as outputs
+	mcp.portMode(MCP23017Port::A, 0);
+	mcp.portMode(MCP23017Port::B, 0);
+	// Configure remaining output pins
+	for (int i = 16; i < 20; i++) {
+		pinMode(pinArray[i], OUTPUT);
+	}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	
+}
 
+void scanPressureTaps() {
+	for (int i = 0; i < 20; i++) {
+		if (i > 15) {
+			digitalWrite(pinArray[i], 1);
+			delay(1000);
+			digitalWrite(pinArray[i], 0);
+		} else {
+			mcp.digitalWrite(pinArray[i], 1);
+			delay(1000);
+			mcp.digitalWrite(pinArray[i], 0);
+		}
+	}
 }
