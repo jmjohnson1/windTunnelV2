0d11cd76c3c37925a4330d677f5329c67787e747
jmjohnson1 <106408381+jmjohnson1@users.noreply.github.com>
2023-07-23 08:10:11 -0500

    ignore

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -24,11 +24,9 @@ const uint8_t sol19_pin = 2;
 const uint8_t sol20_pin = 3;
 
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
